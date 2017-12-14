5b80e93dbbcba1f7c18c6933f3ccf2857e75a13d
Michael_ma <Michael_ma123@126.com>
2017-12-14 20:46:23 +0800

    hash进行MD5加密时必须用utf-8编码

--- project-2/ControlNode/NodeManager.py
+++ project-2/ControlNode/NodeManager.py
@@ -104,7 +104,7 @@ if __name__=='__main__':
     node = NodeManager()
     manager = node.start_Manager(url_q,result_q)
     #创建URL管理进程、 数据提取进程和数据存储进程
-    url_manager_proc = Process(target=node.url_manager_proc, args=(url_q,conn_q,'http://baike.baidu.com/view/284853.htm'.encode('utf-8'),))
+    url_manager_proc = Process(target=node.url_manager_proc, args=(url_q,conn_q,'http://baike.baidu.com/view/284853.htm',))
     result_solve_proc = Process(target=node.result_solve_proc, args=(result_q,conn_q,store_q,))
     store_proc = Process(target=node.store_proc, args=(store_q,))
     #启动3个进程和分布式管理器
